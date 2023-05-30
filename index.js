e41363776f8cd42aa3c2f88557d98c35bbcea3c3
Koshun-code <sit.com.k@gmail.com>
2023-05-30 15:19:46 +0300

    fix 2

--- index.js
+++ index.js
@@ -2,12 +2,11 @@ export default (content) => {
   const [, ...data] = content.split('\n');
   console.log(`Число записей: ${data.length}`);
   const words = data.map((item) => item.split('|'));
-  const districts = words.map((item) => item.at(-2));
+  const districtsA = words.map((item) => item.at(-2));
+  const districts = districtsA.map((district) => district.trim());
   console.log(districts.sort());
   const squaresAS = words.map(word => word.at(3))
   const squares = squaresAS.map((item) => parseInt(item.split(' ').join('')));
-  
-  console.log(squares);
   const square = Math.max(...squares);
   console.log(`Наибольшая площадь: ${square} кв.км `)
   const peoples = 
