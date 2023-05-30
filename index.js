0dc1980ce11c36289f559c674f479e5056c38fe3
Koshun-code <sit.com.k@gmail.com>
2023-05-30 15:23:32 +0300

    firx 2

--- index.js
+++ index.js
@@ -4,11 +4,9 @@ export default (content) => {
   const words = data.map((item) => item.split('|'));
   const districtsA = words.map((item) => item.at(-2));
   const districts = districtsA.map((district) => district.trim());
-  console.log(districts.sort());
+  console.log(`Список округов: ${districts.sort().join(', ')}`);
   const squaresAS = words.map(word => word.at(3))
   const squares = squaresAS.map((item) => parseInt(item.split(' ').join('')));
   const square = Math.max(...squares);
   console.log(`Наибольшая площадь: ${square} кв.км `)
-  const peoples = 
-  console.log();
 }
