99e7a31cc7323ca1e052ea1e2a84d593ad127aa2
Koshun-code <sit.com.k@gmail.com>
2023-05-30 15:00:34 +0300

    Change 3-th

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 export default (content) => {
   const [, ...data] = content.split('\n');
-  //console.log(`Число записей: ${data.length}`);
+  console.log(`Число записей: ${data.length}`);
   const words = data.map((item) => item.split('|'));
   const districts = words.map((item) => item.at(-2));
-  //console.log(districts.sort());
+  console.log(districts.sort());
   const squaresAS = words.map(word => word.at(3))
   const squares = squaresAS.map((item) => parseInt(item.split(' ').join('')));
   
