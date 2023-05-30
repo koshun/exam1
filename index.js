556d983154c67a565274ca8433b65c15d4569141
Koshun-code <sit.com.k@gmail.com>
2023-05-30 15:30:30 +0300

    fix 3asda

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+import _ from 'lodash';
+
 export default (content) => {
   const [, ...data] = content.split('\n');
   console.log(`Число записей: ${data.length}`);
   const words = data.map((item) => item.split('|'));
-  const districtsA = words.map((item) => item.at(-2));
+  const districtsA = _.uniq(words.map((item) => item.at(-2)));
   const districts = districtsA.map((district) => district.trim());
   console.log(`Список округов: ${districts.sort().join(', ')}`);
   const squaresAS = words.map(word => word.at(3))
