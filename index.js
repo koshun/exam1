ecb978d279b942233033fde1001c9ebebbefc079
Koshun-code <sit.com.k@gmail.com>
2023-05-30 14:47:52 +0300

    made 2 item

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-
+export default (content) => {
+  const [, ...data] = content.split('\n');
+  console.log(`Число записей: ${data.length}`);
+  const words = data.map((item) => item.split('|'));
+  const districts = words.map((item) => item.at(-2));
+  console.log(districts.sort());
+  
+}
